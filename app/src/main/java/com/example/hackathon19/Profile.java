15fc964169a51eafc133208b317f46d7f77e9c75
Jappel97 <32598921+Jappel97@users.noreply.github.com>
2019-04-13 17:20:13 -0500

    Made placeholder functions for various views. Just need to fill TODO with firebase stuff

--- app/src/main/java/com/example/hackathon19/Profile.java
+++ app/src/main/java/com/example/hackathon19/Profile.java
@@ -3,45 +3,78 @@ package com.example.hackathon19;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 
 public class Profile extends AppCompatActivity {
 
-    public static final int PICK_IMAGE = 1;
+    public static final int RESULT_LOAD_IMG = 1;
+    private ImageView avatar;
+    private Button saveButton;
+    private TextView nametxt, emailtxt, pwtxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        this.avatar =(ImageView)this.findViewById(R.id.currAvatar);
+        this.saveButton = (Button)this.findViewById(R.id.saveBtn);
+        this.nametxt = (TextView)this.findViewById(R.id.nameText);
+        this.emailtxt = (TextView)this.findViewById(R.id.emailText);
+        this.pwtxt = (TextView)this.findViewById(R.id.pwText);
     }
 
-    void onGetImageClick()
+    public void onGetImageClick(View v)
     {
-        Intent getIntent = new Intent(Intent.ACTION_GET_CONTENT);
-        getIntent.setType("image/*");
-
-        Intent pickIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        pickIntent.setType("image/*");
-
-        Intent chooserIntent = Intent.createChooser(getIntent, "Select Image");
-        chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Intent[] {pickIntent});
-
-        startActivityForResult(chooserIntent, PICK_IMAGE);
+        Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+        photoPickerIntent.setType("image/*");
+        startActivityForResult(photoPickerIntent, RESULT_LOAD_IMG);
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data)
-    {
-        super.onActivityResult(requestCode, resultCode, data);
+    protected void onActivityResult(int reqCode, int resultCode, Intent data) {
+        super.onActivityResult(reqCode, resultCode, data);
 
-        if (requestCode == PICK_IMAGE) {
-            //TODO: action
+        if (resultCode == RESULT_OK) {
+            try {
+                Uri picture = data.getData();
+
+                Bitmap bmp = null;
+                bmp = MediaStore.Images.Media.getBitmap(this.getContentResolver(), picture);
+                avatar.setImageBitmap(bmp);
+            }
+            catch (FileNotFoundException e) {
+                e.printStackTrace();
+                Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG).show();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+                Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG).show();
+            }
+
         }
+        else {
+            Toast.makeText(this, "You haven't picked Image",Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void onSaveBtnClick(View v){
+        //TODO: firebase email set to this.emailtxt
+        //TODO: firebase name set to this.nametxt
+        //TODO: firebase P/W set to this.pwtxt
+        //TODO: firebase picture set to this.avatar
+        this.finish();
     }
 }
--- app/src/main/java/com/example/hackathon19/homescreen.java
+++ app/src/main/java/com/example/hackathon19/homescreen.java
@@ -4,13 +4,22 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class homescreen extends AppCompatActivity {
+
+    private TextView nameTxt;
+    private ImageView avatar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homescreen);
+        this.nameTxt = (TextView)this.findViewById(R.id.nameTV);
+        this.avatar = (ImageView)this.findViewById(R.id.homePfp);
+        //TODO: this.nameTxt = firebase name
+        //TODO: this.avatar = firebase picture
     }
 
     public void onProfileBtnPressed(View v)
