06b681c4b22df8a9f0be6e2b07c7fa9476d72c0d
Mouath Ibrahim <gpg@mouath.com>
2019-04-13 16:42:10 -0500

    refactor classes names, they should be SnakeSnake

--- app/src/main/java/com/example/hackathon19/EditCow.java
+++ app/src/main/java/com/example/hackathon19/EditCow.java
@@ -1,9 +1,9 @@
 package com.example.hackathon19;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class editcow extends AppCompatActivity {
+public class EditCow extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/example/hackathon19/HomeScreen.java
+++ app/src/main/java/com/example/hackathon19/HomeScreen.java
@@ -1,9 +1,9 @@
 package com.example.hackathon19;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class homescreen extends AppCompatActivity {
+public class HomeScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
