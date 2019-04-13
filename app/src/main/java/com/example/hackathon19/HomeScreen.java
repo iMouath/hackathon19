af7b28e465e448e5ad0bcfb0d730f134eb981c00
Jappel97 <32598921+Jappel97@users.noreply.github.com>
2019-04-13 18:46:50 -0500

    Added some more TODO

--- app/src/main/java/com/example/hackathon19/HomeScreen.java
+++ app/src/main/java/com/example/hackathon19/HomeScreen.java
@@ -2,12 +2,22 @@ package com.example.hackathon19;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class HomeScreen extends AppCompatActivity {
+
+    private TextView name;
+    private ImageView pFP;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homescreen);
+        this.name = (TextView)this.findViewById(R.id.nameTV);
+        this.pFP = (ImageView)this.findViewById(R.id.homePfp);
+
+        //TODO: this.name set to firebase name
+        //TODO: this.pFP set to firebase picture
     }
 }
