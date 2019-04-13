5661fd62236b97354804d98af165979798ac3d2e
Mouath Ibrahim <gpg@mouath.com>
2019-04-13 11:30:27 -0500

    firebase login

--- app/src/main/java/com/example/hackathon19/MainActivity.java
+++ app/src/main/java/com/example/hackathon19/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.hackathon19;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.FirebaseApp;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        FirebaseApp.initializeApp(this);
+
+        FirebaseApp firebaseApp = FirebaseApp.initializeApp(MainActivity.this);
+
+        if (firebaseApp != null) {
+            System.out.println(firebaseApp.getOptions());
+        }
+
     }
+    public void onExitButtonClicked(View v)
+    {
+        Button b = (Button)v;
+        Intent myIntent = new Intent(MainActivity.this, LoginActivity.class);
+//        myIntent.putExtra("key", value); //Optional parameters
+        MainActivity.this.startActivity(myIntent);
+    }
+
+
 }
