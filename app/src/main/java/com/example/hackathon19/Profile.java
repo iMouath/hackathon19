5fed05d85fe71349e9eaebc3fd512dda32bb5274
Jappel97 <32598921+Jappel97@users.noreply.github.com>
2019-04-13 15:48:44 -0500

    Made basic image getter, hooked up a button with a view

--- app/src/main/java/com/example/hackathon19/Profile.java
+++ app/src/main/java/com/example/hackathon19/Profile.java
@@ -1,13 +1,47 @@
 package com.example.hackathon19;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class Profile extends AppCompatActivity {
+
+    public static final int PICK_IMAGE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+    }
+
+    void onGetImageClick()
+    {
+        Intent getIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        getIntent.setType("image/*");
+
+        Intent pickIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        pickIntent.setType("image/*");
+
+        Intent chooserIntent = Intent.createChooser(getIntent, "Select Image");
+        chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Intent[] {pickIntent});
+
+        startActivityForResult(chooserIntent, PICK_IMAGE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE) {
+            //TODO: action
+        }
     }
 }
--- app/src/main/java/com/example/hackathon19/homescreen.java
+++ app/src/main/java/com/example/hackathon19/homescreen.java
@@ -1,7 +1,9 @@
 package com.example.hackathon19;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class homescreen extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class homescreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homescreen);
+    }
+
+    public void onProfileBtnPressed(View v)
+    {
+        Intent i = new Intent(this, Profile.class);
+        this.startActivity(i);
     }
 }
