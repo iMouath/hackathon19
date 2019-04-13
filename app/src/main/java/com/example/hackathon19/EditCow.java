e1875c8d11b50fadd8554a825b44805ca35984d2
Jappel97 <32598921+Jappel97@users.noreply.github.com>
2019-04-13 17:42:07 -0500

    Updated to work

--- app/src/main/java/com/example/hackathon19/EditCow.java
+++ app/src/main/java/com/example/hackathon19/EditCow.java
@@ -2,12 +2,28 @@ package com.example.hackathon19;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class EditCow extends AppCompatActivity {
+
+    private Button submitBtn;
+    private TextView myCow;
+    private String cowstring;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editcow);
+        this.submitBtn = (Button)this.findViewById(R.id.cowSubmitBtn);
+        this.myCow = (TextView)this.findViewById(R.id.Cowtxt);
+        cowstring = myCow.toString();
+    }
+
+    public void onUploadPressed(View v){
+        //TODO: firebase cow set as this.cowstring
     }
 }
