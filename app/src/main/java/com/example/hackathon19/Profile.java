b422b3fcb2a5f79ae956ab7a5e0dad5045c8971d
Mouath Ibrahim <gpg@mouath.com>
2019-04-13 19:06:30 -0500

    refactor classes names, they should be SnakeSnake

--- app/src/main/java/com/example/hackathon19/Profile.java
+++ app/src/main/java/com/example/hackathon19/Profile.java
@@ -1,13 +1,11 @@
 package com.example.hackathon19;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -16,7 +14,7 @@ import android.widget.Toast;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
+
 
 public class Profile extends AppCompatActivity {
 
